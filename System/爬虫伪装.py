cdb1eca1844225d2f6c5a0607e8dbda55caecc7b
Elfsong <dumingzhe@126.com>
2016-01-19 18:56:29 +0800

    伪装爬虫

---
+++ System/爬虫伪装.py
@@ -0,0 +1,33 @@
+#!/uesr/Desktop
+#coding:utf-8
+
+import urllib
+import urllib2
+
+def save(filename, contents): 
+    fh = open(filename, 'w') 
+    fh.write(contents) 
+    fh.close()
+
+if __name__ == '__main__':
+    try:
+        class_number = raw_input("请输入查询班级号：")
+        url = 'http://csujwc.its.csu.edu.cn/XSXJ/FB_BJXS_rpt.aspx?Sel_BJ=' + class_number
+        print url
+        user_agent = 'MOzilla/4.0 (compatible;MSIE 5.5;Windows NT)'
+        values = {'name':'DMZ',
+                  'location':'CSU',
+                  'language':'Python'}
+        data = urllib.urlencode(values)
+        headers = {'User-Agent':user_agent}
+
+        req = urllib2.Request(url,data,headers)
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+        
+        save('result.txt', the_page)
+        print 'OK!'
+        print 'Info():'  
+        print response.info()  
+    except urllib2.URLError,e:
+        print e
