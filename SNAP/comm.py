a1b52bef2e247ebdd0be818c7568b3032a5813b0
elfsong <dumingzhe@126.com>
2016-03-30 21:17:38 +0800

    SNAP upgrude

--- SNAP/comm.py
+++ SNAP/comm.py
@@ -14,3 +14,22 @@ def get_url_info(url, seg):
 	if(seg == 4):
 		return url_info.domain+'.'+url_info.suffix
 
+def set_logging():
+	import logging
+	logging.basicConfig(level=logging.DEBUG,
+						format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+						datefmt='%a, %d %b %Y %H:%M:%S',
+						filename='spider.log',
+						filemode='w')
+	console = logging.StreamHandler()
+	console.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+	console.setFormatter(formatter)
+	logging.getLogger('').addHandler(console)
+
+def get_302_url(url):
+	import urllib2,socket
+	request = urllib2.urlopen(urllib2.Request(url, headers = {'User-Agent':'Mozilla/8.0 (compatible; MSIE 8.0; Windows 7)'}))
+	#socket.setdefaulttimeout(30)
+	return request.url
+
