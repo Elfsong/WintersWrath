0e923dea2c7175bdf6738d9446727f6e0cada555
dumingzhe.xyz <dumingzhe@126.com>
2016-08-21 11:20:30 +0800

    服务端完成redis消息队列以及单点推送

--- realtime_server/index.js
+++ realtime_server/index.js
@@ -2,14 +2,31 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// redis 链接
+var redis   = require('redis');
+var client  = redis.createClient('6379', '127.0.0.1');
+
+// redis 链接错误
+client.on("error", function(error) {
+		console.log(error);
+});
+
+//redis准备完毕
+client.on('ready',function(err){
+    console.log('ready');
+});
+
+//服务器应答
 app.get('/', function(req, res){
-		res.send('<h1>Welcome to Realtime Server</h1>');
+		res.send('<h1>Welcome Realtime Server</h1>');
 		});
 
 //在线用户
 var onlineUsers = {};
 //当前在线人数
 var onlineCount = 0;
+//发送消息条数
+var messageCount = 0;
 
 io.on('connection', function(socket){
 		console.log('a user connected');
@@ -28,6 +45,22 @@ io.on('connection', function(socket){
 
 			//向所有客户端广播用户加入
 			io.emit('login', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+
+			for(var i = messageCount - 10; i <= messageCount; i++) {
+				client.hgetall(i, function(err, reply) {
+					if(!isEmpty(reply)) {
+						//用户id
+						console.log(reply.id);
+						//用户昵称
+						console.log(reply.name);
+						//消息内容
+						console.log(reply.content);
+						//向新加入的用户推送之前的消息
+						io.sockets.connected[socket.id].emit('message', {userid:reply.id, username:reply.name, content:reply.content});
+					}
+				});
+			}
+
 			console.log(obj.username+'加入了聊天室');
 			});
 
@@ -35,17 +68,17 @@ io.on('connection', function(socket){
 		socket.on('disconnect', function(){
 			//将退出的用户从在线列表中删除
 			if(onlineUsers.hasOwnProperty(socket.name)) {
-			//退出用户的信息
-			var obj = {userid:socket.name, username:onlineUsers[socket.name]};
+				//退出用户的信息
+				var obj = {userid:socket.name, username:onlineUsers[socket.name]};
 
-			//删除
-			delete onlineUsers[socket.name];
-			//在线人数-1
-			onlineCount--;
+				//删除
+				delete onlineUsers[socket.name];
+				//在线人数-1
+				onlineCount--;
 
-			//向所有客户端广播用户退出
-			io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-			console.log(obj.username+'退出了聊天室');
+				//向所有客户端广播用户退出
+				io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+				console.log(obj.username+'退出了聊天室');
 			}
 		});
 
@@ -54,10 +87,26 @@ io.on('connection', function(socket){
 			//向所有客户端广播发布的消息
 			io.emit('message', obj);
 			console.log(obj.username+'说：'+obj.content);
+			
+			//增加消息计数
+			messageCount += 1;
+			console.log(messageCount);
+
+			//写入redis
+			client.hmset(messageCount, {'id':obj.userid,'name':obj.username,'content':obj.content}, redis.print);
 		});
 
-});
+	});
 
+//监听端口
 http.listen(3000, function(){
-	console.log('listening on *:3000');
-});
+		console.log('listening on *:3000');
+		});
+
+//判断对象是否为空
+function isEmpty(obj) {
+	for (var name in obj) {
+		return false;
+	}
+	return true;
+};
