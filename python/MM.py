227f62a7d0567706593d154612781ea0beab211e
Elfsong <dumingzhe@126.com>
2016-12-28 16:57:53 +0000

    change the jiandan spider tool

--- python/MM.py
+++ python/MM.py
@@ -10,11 +10,8 @@ import urllib2
 from termcolor import colored
 import multiprocessing
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
 def for_one_page_test( url ):
-	#driver.get( url )
-	#driver.find_element_by_id("the-submit").click() 
 
 	#opener = urllib2.build_opener(urllib2.ProxyHandler({'http':'218.244.149.184:8888'}), urllib2.HTTPHandler(debuglevel=1))
 	#urllib2.install_opener(opener)
@@ -27,7 +24,6 @@ def for_one_page_test( url ):
 
 	i_headers = {"User-Agent": "Mozilla/8.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1) Gecko/20090624 Firefox/3.5","Referer": 'http://jiandan.net'}
      	req = urllib2.Request(url, headers=i_headers)
-	#html = driver.page_source.encode('utf8')
 	html = urllib2.urlopen(req).read()
 	print colored( html , 'blue')
 	soup = BeautifulSoup(html)
@@ -36,18 +32,18 @@ def for_one_page_test( url ):
 		if ( isinstance(img_address.get('class'), (list,str) ) and img_address.get('class')[0] == "view_img_link" ):
 			try:			
 				img_url = img_address.get('href')
-				print colored( img_url , 'green')
+                                print colored( img_url[2:] , 'green')
 				image_downlode( img_url )
-			except:
-				print ("Cannot get attr!")
-				continue
+
+                        except Exception,e:  
+                                print Exception,":",e
 			
 
 def image_downlode( url ):
-	img = requests.get( url )
-	name = get_name(url)
+        img = requests.get( 'http://' + url[2:] )
+        name = get_name(url) + '.' + url[-3:] 
 	try:
-		open('/mydata/jiandan9/'+name,'wb').write(img._content)
+		open('/home/elfsong/image/'+name,'wb').write(img._content)
 		print ( name + " done!")
 	except e:
 		print e
@@ -62,10 +58,8 @@ def get_name( url ):
 
 if __name__ == "__main__":
 	start = 1200 
-	end = 1250        
+	end = 1201        
 
-	#driver = webdriver.PhantomJS('/home/erik/source/phantomjs-2.1.1/bin/phantomjs')
-	
 	pool = multiprocessing.Pool(processes = 8)
 
 	btime = time.time()
